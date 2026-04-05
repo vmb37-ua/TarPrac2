0b17c8f91c7562fa466540790835efb6c1d6968a
yy23-ua <yy23@alu.ua.es>
2026-04-05 13:56:26 +0200

    ej 4 hecho

--- ros2_ws/install/mi_accion/share/mi_accion/package.sh
+++ ros2_ws/install/mi_accion/share/mi_accion/package.sh
@@ -80,6 +80,7 @@ _colcon_package_sh_source_script() {
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/mi_accion/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/mi_accion/hook/ament_prefix_path.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/../../build/mi_accion/share/mi_accion/hook/pythonpath_develop.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
